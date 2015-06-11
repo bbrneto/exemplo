a35c89d750400dd2889e08c1c472ed0b942efd55
Benedito Barbosa <bbrneto@gmail.com>
2015-06-10 22:48:15 -0300

    Projeto de exemplo do VRaptor 4

---
+++ src/main/java/br/com/caelum/vraptor/exemplo/controllers/PrimeiroController.java
@@ -0,0 +1,20 @@
+package br.com.caelum.vraptor.exemplo.controllers;
+
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Result;
+
+@Controller
+public class PrimeiroController {
+
+	@Inject
+	private Result result;
+
+	@Get("/home")
+	public void home() {
+		result.include("mensagem", "Olá, VRaptor 4!");
+	}
+	
+}
